79f93c6418cc00d867a24e807ff5183c4a4e7774
Czupkecz <kiloekotango@gmail.com>
2023-02-08 21:27:44 +0100

    Zadanie domowe dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Daniello!");
+﻿// Zadanie domowe dzień 4
+
+var imie = "Marian";
+bool płećKobieta = false;
+var wiek = 29;
+
+if (płećKobieta == true && wiek < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (imie == "Ewa" && płećKobieta == true)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (wiek < 18 && płećKobieta != true)
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
+
