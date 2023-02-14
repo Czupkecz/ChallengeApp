24cdeed2803e4658fd4487049376cc093418f1c8
Czupkecz <kiloekotango@gmail.com>
2023-02-14 07:52:09 +0100

    Zadanie czy liczba jest dodatnia, ujemna lub równa 0

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,29 +1,19 @@
-﻿//Napisz program w C#, który znajdzie maksymalną liczbę spośród trzech podanych liczb
-Console.WriteLine("Sprawdźmy, która z podanych 3 liczb jest największa" + "\n" + "Podaj pierwszą liczbę i naciśnij ENTER");
+﻿//Napisz program w C#, który sprawdzi czy liczba jest dodatnia, ujemna lub równa 0
+Console.WriteLine("Sprawdźmy, czy podana liczna jest dodatnia, ujamna bądź równa 0" + "\n" + "Podaj liczbę i naciśnij ENTER");
 
 string number1 = Console.ReadLine();
 
-Console.WriteLine("Teraz podaj drugą liczbę i naciśnij ENTER");
-
-string number2 = Console.ReadLine();
-
-Console.WriteLine("I ostatnią -> ENTER");
-
-string number3 = Console.ReadLine();
-
 int number1AsInteger = int.Parse(number1);
-int number2AsInteger = int.Parse(number2);
-int number3AsInteger = int.Parse(number3);
 
-if (number1AsInteger > number2AsInteger && number1AsInteger > number3AsInteger)
+if (number1AsInteger > 0)
 {
-    Console.WriteLine("\n Największa liczba to:" + number1);
+    Console.WriteLine("\n Liczba " + number1 + " jest większa od 0");
 }
-else if (number2AsInteger > number1AsInteger && number2AsInteger > number3AsInteger)
+else if (number1AsInteger == 0)
 {
-    Console.WriteLine("\n Największa liczba to:" + number2);
+    Console.WriteLine("\n Liczba " + number1 + " jest równa 0");
 }
 else
 {
-    Console.WriteLine("\n Największa liczba to: " + number3);
+    Console.WriteLine("\n Liczba " + number1 + " jest mniejsza od 0");
 }
